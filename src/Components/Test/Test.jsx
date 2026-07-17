7543ad2c7ed40faaa7e053250468ddbe50931c43
Rajeev Aanjana <rajeevanjana143@gmail.com>
2026-07-17 15:16:50 +0530

    Practice code for Drag and Drop kanban board

--- src/Components/Test/Test.jsx
+++ src/Components/Test/Test.jsx
@@ -1,56 +1,92 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+const INITIAL_STATE = {
+  Todo: ["Task 1", "Task 2", "Task 3"], 
+  "In Progress": ["Task 4", "Task 5"],
+  Completed: ["Task 8", "Task 9"],
+};
 
 const Test = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(INITIAL_STATE);
   const [input, setInput] = useState("");
-  const [focus, setFocus] = useState(false);
 
-  const fetchData = async () => {
-    console.log(input);
-    const res = await fetch(`https://dummyjson.com/recipes/search?q=${input}`);
-    const json = await res.json();
-    setData(json?.recipes);
+  const stateRef = useRef();
+  const taskref = useRef();
+
+  const handleDragStart = (e, item, state) => {
+    stateRef.current = state;
+    taskref.current = item;
+    e.target.style.opacity = "0.5";
   };
+
+  const handleDragEnd = (e) => {
+    e.target.style.opacity = "1";
+  };
+
+  const handleDrop = (e, targetState) => {
+    const sourceState = stateRef.current;
+    const item = taskref.current;
+    setData((prev) => {
+      const newData = { ...prev };
+      newData[sourceState] = newData[sourceState].filter(
+        (task) => task != item,
+      );
+      newData[targetState] = [...newData[targetState], item];
+      return newData;
+    });
+  };
+
+const handleTaskSubmit = ()=>{
+  if(!input.trim()) return
+  setData(prev=> ({...prev, Todo:[...prev.Todo, input]}))
+  setInput("")
+}
 
   useEffect(() => {
-    const timer = setTimeout(fetchData, 300);
-    return () => clearTimeout(timer);
-  }, [input]);
-
-  const selectSearch = (item) => {
-    setInput(item);
-    setFocus(false)
-  };
+    console.log(data);
+  }, [data]);
   return (
-    <>
-      <div className="text-center py-10">
-        <div>
-          <input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            type="text"
-            placeholder="search here..."
-            className="border w-100 p-2"
-            onBlur={() => setFocus(false)}
-            onFocus={() => setFocus(true)}
-          />
-          {focus && data.length > 0 && (
-            <div className="h-100 overflow-auto w-100 border m-auto"
-            onMouseDown={(e)=>e.preventDefault()}>
-              {data.map((item) => (
-                <div
-                  key={item.id}
-                  className="text-left p-2 cursor-pointer hover:bg-gray-200"
-                  onClick={() => selectSearch(item.name)}
-                >
-                  <span className="">{item.name}</span>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
+    <div className="p-10 text-center">
+      <div>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          className="w-100 p-1 mb-10 border"
+          placeholder="wirte here...."
+        />
+        <button onClick={handleTaskSubmit} className="border p-1 mx-2 hover:bg-gray-300 cursor-pointer">
+          Add Task
+        </button>
       </div>
-    </>
+      <div className="flex justify-around">
+        {Object.keys(data).map((state, indx) => (
+          <div
+            key={indx}
+            onDrop={(e) => {
+              handleDrop(e, state);
+            }}
+            onDragOver={(e) => e.preventDefault()}
+            className="bg-gray-300 w-80 p-5"
+          >
+            <h1 className="font-bold text-2xl my-5">{state}</h1>
+            {data[state].map((item, indx) => (
+              <div
+                draggable
+                onDragStart={(e) => {
+                  handleDragStart(e, item, state);
+                }}
+                onDragEnd={(e) => handleDragEnd(e)}
+                key={indx}
+                className="p-2 bg-white my-2 cursor-move"
+              >
+                <h2>{item}</h2>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
