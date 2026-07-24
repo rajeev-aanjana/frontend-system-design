dbabef96af3370a72159bfa87a9a3d72468964a4
Rajeev Aanjana <rajeevanjana143@gmail.com>
2026-07-24 22:43:00 +0530

    Today I have just practiced the previos task like fileexplorer, nested check boxes and all

--- src/Components/Test/Test.jsx
+++ src/Components/Test/Test.jsx
@@ -1,49 +1,141 @@
-import React, { useState } from "react";
+import React, { Children, useEffect, useState } from "react";
 
-const Test = () => {
-  const [data, setData] = useState([]);
-  const [input, setInput] = useState("");
+const CHECKBOX_DATA = [
+  {
+    id: 1,
+    name: "Parent-1",
+    children: [
+      {
+        id: 2,
+        name: "Parent-2",
+        children: [
+          {
+            id: 3,
+            name: "Child-1",
+          },
+          {
+            id: 4,
+            name: "Child-2",
+          },
+          {
+            id: 5,
+            name: "Child-3",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: "Parent-3",
+    children: [
+      {
+        id: 7,
+        name: "Parent-4",
+        children: [
+          {
+            id: 133,
+            name: "Extra Child",
+          },
+          {
+            id: 8,
+            name: "Parent-5",
+            children: [
+              { id: 9, name: "Child-4" },
+              { id: 10, name: "Child-5" },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 12,
+    name: "Parent-6",
+    children: [
+      {
+        id: 11,
+        name: "Child-7",
+      },
+    ],
+  },
+];
 
-  const handleAdd = (e) => {
-    if (e.key !== "Enter" || !input.trim()) return;
+const CheckBox = ({ checkboxData, checked, setChecked }) => {
+  const handleChange = (isChecked, node) => {
+    setChecked((prev) => {
+      const newState = { ...prev, [node.id]: isChecked };
 
-    setData((prev) => [...prev, { id: Date.now(), name: input.trim() }]);
-    setInput("");
-  };
+      const updateChild = (node) => {
+        node.children?.forEach((child) => {
+          newState[child.id] = isChecked;
+          if (child?.children) {
+            updateChild(child);
+          }
+        });
+      };
 
-  const deletTask = (id) => {
-    console.log(id)
-    setData((prev) => prev.filter((item) => item.id !== id));
+      updateChild(node);
+
+      const updateParent = (node) => {
+        if (!node.children) return newState[node.id] || false;
+
+        const verifyAll = node.children.every((child) => updateParent(child));
+        newState[node.id] = verifyAll;
+        return verifyAll;
+      };
+
+      CHECKBOX_DATA.forEach((node) => updateParent(node));
+
+      // const verifyChecked = (node) => {
+      //   if (!node.children) return newState[node.id] || false;
+
+      //   const isAllChecked = node.children.every((child) =>
+      //     verifyChecked(child),
+      //   );
+
+      //   newState[node.id] = isAllChecked;
+
+      //   return isAllChecked;
+      // };
+
+      // CHECKBOX_DATA.forEach((node) => verifyChecked(node));
+      return newState;
+    });
   };
   return (
-    <div className="flex justify-center items-center h-100 w-full">
-      <div>
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => handleAdd(e)}
-          autoFocus
-          type="text"
-          placeholder="type here..."
-          className="border w-100 p-2"
-        />
-        <div className="grid grid-cols-3 gap-4 pt-5">
-          {data.map((item) => (
-            <div
-              key={item.id}
-              className="bg-gray-400 p-2 px-4 flex justify-between rounded-3xl"
-            >
-              <span>{item.name}</span>{" "}
-              <span
-                className="text-sm cursor-pointer"
-                onClick={() => deletTask(item.id)}
-              >
-                ❌
-              </span>
-            </div>
-          ))}
+    <div>
+      {checkboxData.map((node) => (
+        <div className="px-4" key={node.id}>
+          <input
+            className="cursor-pointer"
+            type="checkbox"
+            checked={checked[node.id] || false}
+            onChange={(e) => handleChange(e.target.checked, node)}
+          />
+          <span className="px-2">{node.name}</span>
+          {node?.children && (
+            <CheckBox
+              checkboxData={node.children}
+              checked={checked}
+              setChecked={setChecked}
+            />
+          )}
         </div>
-      </div>
+      ))}
+    </div>
+  );
+};
+
+const Test = () => {
+  const [checked, setChecked] = useState({});
+  return (
+    <div className="p-10">
+      <CheckBox
+        checkboxData={CHECKBOX_DATA}
+        checked={checked}
+        setChecked={setChecked}
+      />
     </div>
   );
 };
